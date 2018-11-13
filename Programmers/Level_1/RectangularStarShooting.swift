b76eec5583d6bcbd53e36333562bb7a33806ada8
P.O.S <777skyim777@gmail.com>
2018-11-13 10:54:00 +0900

    181113_rectangular star shooting

---
+++ Programmers/Level_1/RectangularStarShooting.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+/// My Solution
+let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+let (a, b) = (n[0], n[1])
+
+for _ in 1...b {
+    for _ in 1...a {
+        print("*", terminator: "")
+    }
+    print("")
+}
+/// Other Solution
+Empty...
