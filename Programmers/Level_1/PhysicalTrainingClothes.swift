8bc5664ec530b719cd7dc7c1659a8e15fd5ef901
P.O.S <777skyim777@gmail.com>
2018-11-22 19:05:55 +0900

    181122_physical training clotes_i didn't check except situations

---
+++ Programmers/Level_1/PhysicalTrainingClothes.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+/// My Solution
+func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
+    let stolenClothStudents: [Bool] = makeStolenClothStudents(studentCount: n, lostNumber: lost)
+    let lendReserveClothes: [Bool] = lendReserveCloth(stolenClothStudents, reserve)
+    return lendReserveClothes.reduce(0, { (result: Int, element: Bool) -> Int in
+        return result + (element == true ? 1 : 0)
+    })
+}
+
+func makeStolenClothStudents(studentCount: Int, lostNumber: [Int]) -> [Bool] {
+    let beforeStolenCloth = Array(repeating: true, count: studentCount)
+    var afterStolenCloth = beforeStolenCloth
+    for (index, item) in beforeStolenCloth.enumerated() {
+        for i in lostNumber {
+            if index+1 == i { afterStolenCloth[index] = false }
+        }
+    }
+    return afterStolenCloth
+}
+func lendReserveCloth(_ stolenClothStudents: [Bool], _ reserve: [Int]) -> [Bool] {
+    var result: [Bool] = stolenClothStudents
+    for item in reserve {
+        for (index, check) in stolenClothStudents.enumerated() {
+            if check == false {
+                if index-1 == item-1 || index+1 == item-1 {
+                    result[index] = true
+                    break
+                }
+            }
+        }
+    }
+    return result
+}
+
